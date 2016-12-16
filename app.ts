f387754e2290cee354279b4d2b5dacddaf52c0ff
Tirth <complaints@tirthpatel.com>
2016-12-16 14:09:39 -0500

    Initial Shelly Bean

--- app.ts
+++ app.ts
@@ -1,23 +1,30 @@
-var Greeter = (function () {
-    function Greeter(element) {
+﻿class Greeter {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+
+    constructor(element: HTMLElement) {
         this.element = element;
         this.element.innerHTML += "The time is: ";
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
     }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
-    };
-    Greeter.prototype.stop = function () {
+
+    start() {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    }
+
+    stop() {
         clearTimeout(this.timerToken);
-    };
-    return Greeter;
-}());
-window.onload = function () {
+    }
+
+}
+
+window.onload = () => {
     // var el = document.getElementById('content');
     // var greeter = new Greeter(el);
     // greeter.start();
+
     console.log("hey");
 };
